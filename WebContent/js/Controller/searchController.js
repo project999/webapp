7a3cdc495a2430817da325ae6f3ebdde71d15e20
Dennis(이민섭) <msaa@coupang.com>
2015-08-15 20:52:54 +0900

    [MSAA-01] detail Template 작업. 연관파일은 아직.

--- WebContent/js/Controller/searchController.js
+++ WebContent/js/Controller/searchController.js
@@ -8,25 +8,23 @@ function searchController($scope, $http) {
 	                   ];
 	  $scope.searchResults;
 	  $scope.search = function() {
-		    $scope.q = $scope.searchKeywd ;
-		    $('#notice-panel').attr('hidden','true');
-		    $('search-result').removeAttr('hidden');
+		  $scope.q = $scope.searchKeywd ;
+		  $('#notice-panel').attr('hidden','true');
+		  $('search-result').removeAttr('hidden');
 		    
-		    $http({
-		    	method: 'get',
-		    	url: 'http://58.229.240.117:8983/solr/gettingstarted/select?q='+$scope.q+'&wt=json&indent=true',
-		    	header: {'content-Type':'appliction/json; charset=utf-8'}
-		    })
-		    .success(function(data){
-		    	//alert("test success");
-		    	$scope.searchResults = data.response.docs;
-		    	href: 'localhost:8080/TestSearchResult.html'
-		    	//alert($scope.searchResults)
-		    })
-		    .error(function(data){
-		    	alert("통신에 오류가 있습니다. 잠시 후 다시 시도해주세요.");
-		    });
-		  };
-	  
-
+		  $http({
+		  	method: 'get',
+		  	url: 'http://58.229.240.117:8983/solr/gettingstarted/select?q='+$scope.q+'&wt=json&indent=true',
+		  	header: {'content-Type':'appliction/json; charset=utf-8'}
+		  })
+		  .success(function(data){
+		  	//alert("test success");
+		  	$scope.searchResults = data.response.docs;
+		  	href: 'localhost:8080/TestSearchResult.html'
+		  	//alert($scope.searchResults)
+		  })
+		  .error(function(data){
+		  	alert("통신에 오류가 있습니다. 잠시 후 다시 시도해주세요.");
+		  });
+		};
   }
